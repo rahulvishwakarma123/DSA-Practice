cf9b2924d156da7307712cc7370834919ae20ec9
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2026-02-14 20:01:25 +0530

    added search in roteted and sorted array

--- DSA QUESTIONS/Leetcode33.java
+++ DSA QUESTIONS/Leetcode33.java
@@ -1,3 +1,44 @@
 public class Leetcode33 {
-    
+    public static int binarySearch(int[] arr, int target, int si, int ei){
+        if(si > ei){
+            return -1;
+        }
+        
+        int mid = si + (ei - si)/2;
+        
+        // found case
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // mid on line1
+        if (arr[si] <= arr[mid]) {
+            // case: a
+            if (arr[si] <= target && target <= arr[mid]) {
+                return binarySearch(arr, target, si, mid-1);
+            }
+            // case: b
+            else{
+                return binarySearch(arr, target, mid+1, ei);
+            }
+        }
+        // mid on line2
+        else{
+            // case: c
+            if (target <= arr[ei] && arr[mid] <= target) {
+                return binarySearch(arr, target, mid+1, ei);
+            }
+            // case: d
+            else{
+                return binarySearch(arr, target, si, mid-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2,3};
+        int target = 0;
+        int ans = binarySearch(arr, target, 0, arr.length -1);
+        System.out.println(ans);
+    }
 }
